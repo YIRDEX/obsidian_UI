8ebdddbd69266482b577e527e4ba1254ec50986e
YIRDEX <3808701817@qq.com>
2026-06-05 20:35:31 +0800

    jhbydrgxuxav
    Updated the color scheme for the 'Default' theme in ThemeManager.

--- ThemeManager.lua
+++ ThemeManager.lua
@@ -49,7 +49,7 @@ do
     ThemeManager.BuiltInThemes = {
         ["Default"] = {
             1,
-            { FontColor = "ffffff", MainColor = "13263D", AccentColor = "1E90FF", BackgroundColor = "0A192F", OutlineColor = "87CEEB" },
+            { FontColor = "ffffff", MainColor = "191919", AccentColor = "7d55ff", BackgroundColor = "0f0f0f", OutlineColor = "282828" },
         },
         ["BBot"] = {
             2,
